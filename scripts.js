bc75517b584feda1797888fe1a2ac577289f6a7b
FJM <frazilpop@googlemail.com>
2019-12-05 17:31:27 +0000

    new arrows and grid

--- scripts.js
+++ scripts.js
@@ -19,6 +19,8 @@ span.onclick = function() {
 } 
 
 
+
+
 $(function(){
   var includes = $('[data-include]');
   jQuery.each(includes, function(){
@@ -35,9 +37,51 @@ $(".slick-slider").slick({
     fade: true,
     cssEase: 'linear' */
 });
+$(".slick-slider-single").slick({
+  dots: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 540,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 $("#menu-toggle").click(function (e) {
   e.preventDefault();
   $("#wrapper").toggleClass("toggled");
 });
 
+var leftArrow = document.getElementsByClassName("slick-prev")[0];
+var rightArrow = document.getElementsByClassName("slick-next")[0];
+
+rightArrow.onclick = function() {
+  leftArrow.style.display = "block";
+  leftArrow.style.setProperty('display', 'block', 'important');
+
+
+} 
